c0c0fe844cc3c2750be4e8d4b0378b480111fbf7
Soudeep Tikadar <108390210+soudeeptikadar@users.noreply.github.com>
2025-01-08 23:22:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# YouTube API Configuration
+YOUTUBE_API_KEY = 'Your YouTube API Key'
+
+# Google Sheets API Configuration
+SHEET_ID = 'Your Google Sheets ID'
+SHEET_NAME = 'Youtube_Update'  # Name of the sheet to send data to
+
+# Google Sheets API credentials file (downloaded from Google Cloud Console)
+GOOGLE_SHEETS_CREDENTIALS_FILE = 'credentials.json'  # Path to your credentials.json file
